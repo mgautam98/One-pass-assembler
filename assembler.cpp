4f12b40244ea20838a7fd71e899b246b43f91521
Gautam <gautam@mishragautam.com>
2019-04-12 06:45:23 +0530

    Add basic functions

--- assembler.cpp
+++ assembler.cpp
@@ -1,17 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Assembler{
-    private:
+inline bool fileExists(const std::string &name)
+{
+    ifstream f(name.c_str());
+    return f.good();
+}
 
+class Assembler
+{
+  private:
+    string src_file_name;
+    string symtab_file_name;
+    string optab_file_name;
+    string object_file_name;
 
-    public:
-    
-    Assembler(){
-
-    }
+  public:
+    Assembler(string src, string optab, string symtab, string obj);
+    void displaySourceCode();
+    void displayOptab();
+    void displayObjectCode();
 };
 
+
+Assembler::Assembler(string src, string optab, string symtab, string obj)
+{
+    src_file_name = src;
+    symtab_file_name = symtab;
+    optab_file_name = optab;
+    object_file_name = obj;
+    if (!fileExists(src_file_name) || !fileExists(optab_file_name))
+    {
+        cout << "\n\t\t SOURCE FILE OR OPTAB DOESN'T EXISTS\n\n";
+        system("sleep 3");
+    }
+}
+
+void Assembler::displaySourceCode(){
+    ifstream ifs(src_file_name.c_str());
+	for(string line; getline(ifs, line);)
+		cout<<line<<endl;
+	return;
+}
+
+void Assembler::displayOptab(){
+    ifstream ifs(optab_file_name.c_str());
+	for(string line; getline(ifs, line);)
+		cout<<line<<endl;
+	return;
+}
+
+void assembleNewProgram()
+{
+    string src, optab, object, symtab;
+    cout << "\n\t\tSource File Name  :  ";
+    cin >> src;
+    cout << "\t\tOPTAB file Name  :  ";
+    cin >> optab;
+    cout << "\t\tFile name where Object Code will be stored  :  ";
+    cin >> object;
+    symtab = "symtab.txt";
+    Assembler(src, optab, symtab, object);
+}
 
 int main()
 {
@@ -26,7 +76,8 @@ int main()
             break;
         switch (inp)
         {
-        case 1: //assemble
+        case 1:
+            assembleNewProgram();
             break;
         default:
             cout << "\n\t\tInvalid Input\n\n";
